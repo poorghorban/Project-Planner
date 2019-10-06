a0264e56c43a873f18989fc49d4acd3e732a9179
Ziba Poorghorban <ziba.poorghorban@gmail.com>
2019-10-06 21:06:14 +0330

    edit __init__.py file

--- project/planner/__init__.py
+++ project/planner/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
 
 app = Flask(__name__)
+engine = create_engine('mysql+mysqlconnector://root:software1393@localhost/db_plan' , encoding="utf8")
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
 
 from .views import * 
