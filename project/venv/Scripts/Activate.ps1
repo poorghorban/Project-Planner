189a9dfc0a202f7183981e766f50b58f532e798d
Poorghorban <ziba.poorghorban@gmail.com>
2019-10-08 11:09:14 +0330

    change venv

--- project/venv/Scripts/Activate.ps1
+++ project/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\z.poorghorban\Documents\Flask\project1\venv"
+$env:VIRTUAL_ENV="C:\Users\z.poorghorban\Documents\GitHub\Project-Planner\project\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- project/venv/Scripts/activate.bat
+++ project/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\z.poorghorban\Documents\Flask\project1\venv"
+set "VIRTUAL_ENV=C:\Users\z.poorghorban\Documents\GitHub\Project-Planner\project\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
