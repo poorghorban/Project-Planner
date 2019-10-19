2880602f4cd43d57a67bf120073b91bae32cd739
Poorghorban <ziba.poorghorban@gmail.com>
2019-10-19 19:35:42 +0330

    edit files

--- project/planner/__init__.py
+++ project/planner/__init__.py
@@ -1,13 +1,34 @@
-from flask import Flask 
+from flask import Flask
 from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session , sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from datetime import timedelta
 
+# create app and config
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret key'
 
-engine = create_engine('mysql+mysqlconnector://root:software1393@localhost/db_plan?charset=utf8' , encoding="utf8")
+# config database
+engine = create_engine('mysql+mysqlconnector://root:software1393@localhost/db_planner?charset=utf8' , encoding="utf8")
+db = scoped_session(sessionmaker(autocommit=False , autoflush=False , bind=engine))
+
 Base = declarative_base()
-Session = sessionmaker(bind=engine)
-session = Session()
+Base.query = db.query_property()
+
+'''
+def init_db():
+    from .models import *
+   Base.metadata.create_all(bind=engine)
+'''
+
+# config upload image 
+UPLOAD_FOLDER = "planner//static//upload_image"
+ALLOWED_EXTENSIONS = set(['png' , 'jpg' , 'jpeg'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+# config session
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 from .views import *
